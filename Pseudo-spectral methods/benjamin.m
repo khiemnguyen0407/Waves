a52d17a0126c47c673dc854c5c27228612903a0f
Khiem Nguyen <51482802+khiemnguyen0407@users.noreply.github.com>
2020-12-05 02:21:20 +0100

    Update benjamin.m

--- Pseudo-spectral methods/benjamin.m
+++ Pseudo-spectral methods/benjamin.m
@@ -1,59 +1,73 @@
-%BENJAMIN  Solve the Benjamin-Ono equation
-%   u_t + u_x + 2*u*u_x - alpha*Hu_{xx} - beta * u_{xxx} = 0
+%BENJAMIN  Solve the Benjamin equation
+%   
+%
+% BENJAMIN implements a numerical scheme for solving the Benjamin equation
+%
+%   u_t + u_x + 2*u*u_x - alpha*Hu_{xx} - beta * u_{xxx} = 0.   (1)
+%
+% As the name suggests, the above equation is derived by Benjamin in [1].
+% The dispersive term of the Benjamin equation contains the dispersion of
+% the Benjamin-Ono type and the KdV type. Particularly, the first
+% dispersive term, namely Hu_{xx}, corresponds to the BO equation and the
+% second, namely u_{xxx}, to the KdV equation. 
+%
+% The script is a complete solver of the Benjamin equation. That is, it
+% starts with setting up a uniform mesh on which the numerical solution
+% will be computed, discretize the solution in space and solve the resulted
+% system of ODEs in time. Unlike the solver for KdV equation (see kdv.m),
+% this equation cannot be solved by other methods such as the finite
+% difference and finite element methods due to the appearance of the
+% Hilbert transform.  The Hilbert transform of function f = f(x) is defined
+% as the convolution of a singular kernel with the function itself. Thus,
+% although it is possible to approximate a Hilbert transform in a numerical
+% basis, it is not trivial to integrate such approximations into a
+% numerical scheme for the wave solution. For more explanation on the
+% numerical strategy, type "help kdv".
+%
+% Equation (1) is a prototype of non-integrable wave equation and thus
+% there is no exact solution of this equation. For this reason, we use the
+% jump initial condition that is typically used to generate the dispersive
+% shock waves. The jump condition is smoothed out by using the
+% regularized tanh function instead of the straight constant lines.
 
-% Space-time mesh of the wave problem
-% The equation is solved on the interval [-L,L].
-L = 2000*pi;  	
-N = 2^14+1;
-disp(['L = ', num2str(L/pi), '*pi']);
-disp(['N = 2^', num2str(length(factor(N-1))), '+1']);
-% Scale factor transforming the Fourier Transform on [-pi,pi] --> [-L,L] 
-scale = L/pi;
-x = scale*(2*pi/N)* (-(N-1)/2 : (N-1)/2);
-k = [0: (N-1)/2,  -(N-1)/2: -1]/scale;
-T = 500;
+%% Space-time mesh
+a = -150;       % left boundary
+b = 150;        % right boundary
+L = b - a; 
+N = 2^10;     % number of modes
+h = L/N;
+scale = L/(2*pi);
+if mod(N, 2) == 0   % define collocation & wave numbers
+    x = (a : h : b-h)';
+    k = [0:N/2-1, 0, -N/2+1 : -1]'/scale;
+else
+    x = (a+h/2 : h : b-h/2)';
+    k = [0:(N-1)/2, -(N-1)/2: -1]'/scale;
+end
+tmax = 20;
 
-%% Parameters for Benjamin equation.
-alpha = -1; % alpha = +1; 
-beta = -1;  % beta = +1;
+alpha = +1; % Parameters for Benjamin equation.
+beta  = +1;
 
-%% Initial condition.
-% For dispersive shock waves.
-uLimit = [1, 0];
-delta = 2.5*ones(1,2);
+% Initial condition: We use the jump condition for generating DSWs.
+jumpLocation = [a, 0];
+uLimits = [1, 0];
+delta = 1.5*ones(1,2);
+iu = 0.5*(uLimits(1) - uLimits(2))*(tanh(delta(1)*(x-jumpLocation(1))) ...
+        - tanh(delta(2)*(x-jumpLocation(2)))) + uLimits(2);
 
-jumpLocation = [-L/2, L/2];
-iu = 0.5*(uLimit(1) - uLimit(2))*(tanh(delta(1)*(x-jumpLocation(1))) ...
-        - tanh(delta(2)*(x-jumpLocation(2)))) + uLimit(2);
-iu = iu(:); % Colum vector.
+%% Time integration in the Fourier space
+tstart = tic;
 
-
-% Numerical integration for wave solution using pseudo-spectral method.
 ik = 1i*k;
 q = -(1i*k + alpha*1i*k.*abs(k) + 1i*beta*k.^3);
-% Put the algorithmic variables into the column vector.
-ik = ik(:);
-q = q(:);
+odefunc = @(t, u_fourier) q.*u_fourier - ik.*fft(ifft(u_fourier).^2);
+tspan = linspace(0, tmax, 51);
+% options = odeset('RelTol', 1e-6, 'AbsTol', 1e-8);
 
-% Setup the right-hand side function f(t,y) in the equation y'(t) = f(t,y)
-% in the Fourier space.
-ODEFUNC = @(t, u_fourier) q.*u_fourier - ik.*fft(ifft(u_fourier).^2);
-tspan = linspace(0, T, 101);
-options = odeset('RelTol', 1e-3, 'AbsTol', 1e-4);
-% Solve the wave equation in Fourier space using ODE45 by MATLAB.
-[t, sol_fourier] = ode45(ODEFUNC, tspan, fft(iu), options);
-% Retrieve the solution in real space.
+[t, sol_fourier] = ode45(odefunc, tspan, fft(iu));
 u = ifft(sol_fourier, [], 2);
+
+telapsed = toc(tstart); fprintf('Time processed: %f \n', telapsed);
 %% Plot the solution.
-close all
-tIndex = 26:25:101;
-figure('Position', [100, 100, 1600, 800])
-for i = 1:length(tIndex)
-    subplot(2,2,i)
-    tt = t(tIndex);
-    xx = linspace(min(x), max(x), 16*(N-1)+1);
-    uu = interp1(x, u(tIndex(i),:), xx, 'spline');
-    plot(xx, uu, 'k-', x, u(1,:), '--', 'LineWidth', 1), grid on
-    text(0, 2.25, ['$t = ', num2str(t(tIndex(i))), '$'], ...
-        'Interpreter', 'latex', 'FontSize', 20, 'HorizontalAlignment', 'center');
-end
+plot(x, u(1,:), 'b-', x, u(end-20,:), 'k-', 'LineWidth', 1.5);
